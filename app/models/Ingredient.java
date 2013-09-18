a1c27fb172cfa99321d5558cc774f834da6b065f
JCook <JCook83@gmail.com>
2013-09-18 02:16:48 -0500

    Adding comments

--- app/models/Ingredient.java
+++ app/models/Ingredient.java
@@ -1,5 +1,6 @@
 package models;
 
+import com.avaje.ebean.Ebean;
 import com.avaje.ebean.annotation.Transactional;
 import play.data.validation.Constraints;
 import play.db.ebean.Model;
@@ -34,7 +35,12 @@ public class Ingredient extends Model {
     @OneToMany(cascade = CascadeType.ALL)
     public List<IngredientAlias> aliases;
 
-    // TODO: join with aliases (separate query?)
+    /**
+     * Find the ingredient with the specified name
+     * @param name The name of the ingredient to find
+     * @return A (non-subtype, non-temporary) Ingredient with the specified name, if available
+     * TODO: join with aliases (separate query?)
+     */
     public static Ingredient findIngredient(String name) {
         return find.fetch("category")
                 .where().eq("name", name)
@@ -42,39 +48,69 @@ public class Ingredient extends Model {
                 .where().eq("isSubtype", "false").findUnique();
     }
 
+    /**
+     * Find all ingredients
+     * @return All (non-subtype, non-temporary) ingredients
+     */
     public static List<Ingredient> findIngredients() {
         return find.fetch("category")
                 .where().eq("isTempItem", "false")
                 .where().eq("isSubtype", "false").findList();
     }
 
+    /**
+     * Find all temporary ingredients
+     * @return All temporary ingredients
+     */
     public static List<Ingredient> findTempIngredients() {
         return find.where().eq("isTempItem", "true").findList();
     }
 
+    /**
+     * Find a subtype ingredient with the specified name
+     * @param name The name of the subtype ingredient to find
+     * @return A subtype Ingredient with the specified name, if available
+     */
     public static Ingredient findSubtype(String name) {
         return find.fetch("supertype")
                 .where().eq("name", name)
                 .where().eq("isSubtype", "true").findUnique();
     }
 
-    public void addSubtype(Ingredient subType) {
+    /**
+     * Add a subtype to this Ingredient
+     * @param subtype An Ingredient to be created as a subtype of this Ingredient
+     */
+    public void addSubtype(Ingredient subtype) {
         if (isSubtype) {
             throw new UnsupportedOperationException("Cannot add a subtype to a subtype");
         }
-        subType.isSubtype = true;
-        subType.supertype = this;
-        subType.save();
+        subtype.isSubtype = true;
+        subtype.supertype = this;
+        subtype.save();
     }
 
+    /**
+     * Save this Ingredient as a new temporary ingredient
+     */
     public void saveAsTempIngredient() {
         isTempItem = true;
         save();
     }
 
     /**
+     * Delete all temporary ingredients
+     */
+    public void deleteTempIngredients() {
+        //TODO: replace with Ebean.delete(Collection) once Play includes bugfix for EBean bug #420
+        for (Ingredient tempIngredient : find.where().eq("isTempItem", true).findList()) {
+            tempIngredient.delete();
+        };
+    }
+
+    /**
      * Workaround for broken cascade deletion for One-To-Many self-relationships in EBean ORM.
-     * TODO: Replace with cascade delete once EBean bug #420 is fixed
+     * TODO: Remove this and replace with normal cascading delete() once Play includes bugfix for EBean bug #420
      */
     @Transactional
     public void deleteIngredientAndSubtypes() {
@@ -85,4 +121,5 @@ public class Ingredient extends Model {
         }
         find.where().eq("id", id).findUnique().delete();
     }
+
 }
